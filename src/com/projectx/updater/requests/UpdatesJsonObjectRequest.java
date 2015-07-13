a86b64b317c9716299da6c3db090953040a5df8c
TheQuiche <yordi.derijcke@live.be>
2015-07-13 19:07:20 +0200

    Fix something that went lost in merging
    Change-Id: I9a698f1289fb0f9b44659a8128c254acb32cf6d4
    Signed-off-by: TheQuiche <yordi.derijcke@live.be>

--- src/com/projectx/updater/requests/UpdatesJsonObjectRequest.java
+++ src/com/projectx/updater/requests/UpdatesJsonObjectRequest.java
@@ -7,7 +7,7 @@
  * or at https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-package com.projectx.requests;
+package com.projectx.updater.requests;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Response;
